20e9bf749e6c27d7cdb7fbb83aeffc988d063f79
Apprentice <apprentice@dbc-chicago-7.local>
2014-09-05 11:18:11 -0500

    scraping wikipedia for image and intro works

--- app/models/species.rb
+++ app/models/species.rb
@@ -3,4 +3,26 @@ class Species < ActiveRecord::Base
 
   validates :scientific_name, uniqueness: true
 
+  # regexs
+  P_START = /<p>/ # start paragraph tags
+  P_END = /<\/p>/ # end paragraph tags
+  A_START = /<a[\s\/=a-zA-Z0-9."]*>/ # start link tag
+  A_END = /<\/a>/ # end link tag
+
+  def parseWikipedia
+    # Working
+    # TODO On error, get Genus info instead
+
+    url = "http://en.wikipedia.org/wiki/#{self.scientific_name.split(" ").join("_")}"
+    p url
+    doc = Nokogiri::HTML(open(url))
+    img_link = doc.search('.infobox img')[0]['src']  # img source
+    intro = (doc.search('p')[0]).to_s  # first paragraph
+    intro.gsub!(P_START, '<div class="intro">')
+    intro.gsub!(P_END, "</div>")
+    intro.gsub!(A_START, "<em>")
+    intro.gsub!(A_END, "</em>")
+    {intro: intro, img: img_link}
+  end
+
 end
