cb30596344f0c9767d855dd23f148e228847a297
Jeff Keslin <jkeslin@gmail.com>
2014-09-05 18:00:05 -0500

    adjusted search to ignore upcase and downcase issues

--- app/controllers/species.rb
+++ app/controllers/species.rb
@@ -13,7 +13,8 @@ end
 
 
 post '/species' do
-  @species = Species.where("common_name LIKE ? OR scientific_name LIKE ?", "%#{params[:species]}%", "%#{params[:species]}%").limit(20)
+
+  @species = Species.where("lower(common_name) LIKE ? OR lower(scientific_name) LIKE ?", "%#{params[:species].downcase}%", "%#{params[:species].downcase}%").limit(20)
   content_type :json
   { species: @species }.to_json
 end
