1d1cd7e650186c6610005b918941793d5f1cc093
dukegreene <dukegreene@gmail.com>
2014-09-06 14:00:48 -0500

    integrated model tests, one index view test, and travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,8 @@ require 'capybara'
 require 'capybara/rspec'
 require 'factory_girl'
 require 'factories'
+require 'faker'
+
        
 Capybara.configure do |config|
   config.run_server = false
@@ -24,7 +26,7 @@ Capybara.app = Sinatra::Application.new
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include Capybara::DSL
-  config.include Factory::Syntax::Methods
+  # config.include Factory::Syntax::Methods
 end
 
 def app
