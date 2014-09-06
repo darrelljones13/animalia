0c7a21a06007cceac6c2e33deb84ff86001828c0
Ryan Spencer <rjspencer82@gmail.com>
2014-09-06 17:48:21 -0500

    changed search to a htpp request with redirect

--- public/js/application.js
+++ public/js/application.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
 	});
 
 	//search for species in database
-	$('#species-search').on('submit', function(event){
-		event.preventDefault();
-		var species = $('input').val();
-		$.post('/species', {species: species}, function(foundSpecies){
-			console.log(foundSpecies)
-		});
-	});
+	// $('#species-search').on('submit', function(event){
+	// 	event.preventDefault();
+	// 	var species = $('input').val();
+	// 	$.post('/species', {species: species}, function(foundSpecies){
+	// 		console.log(foundSpecies)
+	// 	});
+	// });
 
 });//end doc ready
