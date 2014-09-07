86b7bcc4364cd532fd3c61289ec63d0db502a639
Jeff Keslin <jkeslin@gmail.com>
2014-09-07 18:54:42 -0500

    debugged search modals

--- app/controllers/species.rb
+++ app/controllers/species.rb
@@ -19,15 +19,12 @@ end
 
 post '/species' do
   @species = Species.find_by("lower(common_name) LIKE ? OR lower(scientific_name) LIKE ?", "%#{params[:species].downcase}%", "%#{params[:species].downcase}%")
-  puts @species.id
-  # redirect '/species/#{@species.id}'
   content_type :json
   { species: @species }.to_json
 end
 
 post '/species/search' do
   @species = Species.find_by("lower(common_name) LIKE ? OR lower(scientific_name) LIKE ?", "%#{params[:species].downcase}%", "%#{params[:species].downcase}%")
-  puts @species.id
   redirect "/species/#{@species.id}"
 end
 
@@ -68,17 +65,12 @@ get '/species/scrape_wikipedia' do
   redirect "/"
 end
 
-get '/species/:search' do
-  @user = User.find(session[:user_id])
-  if params[:search] == nil
-    redirect '/'
-  elsif params[:search].to_i > 0
-    puts "Search by integer"
-    @species = Species.find(params[:search].to_i)
+get '/species/:search' do |search_result|
+  # @user = User.find(session[:user_id])
+  unless search_result.nil?
+    @species = Species.find(search_result.to_i)
   else
-    puts "Search by name #{params[:search]}"
-    # TODO needs error handling
-    @species = Species.where(common_name: params[:search])
+    redirect '/'
   end
   @relatives = Species.where("genus_id = ? AND id != #{@species.id}", "#{@species.genus_id}").limit(20)
   @taxonomy = @species.taxonomy
