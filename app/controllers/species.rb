167650aa2f5159e0b8915e794c589cb3c5b77229
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-09-09 14:59:35 -0500

    added selection for collections when adding from a species view

--- app/controllers/species.rb
+++ app/controllers/species.rb
@@ -15,9 +15,8 @@ end
 
 #not finished. need to address edge cases
 post '/add_to_collection' do
-  if session[:user_id] != nil
-    @user_collection = current_user.collections.first
-    @card = Card.create(user_id: current_user.id, species_id: params[:species_id], collection_id: @user_collection.id)
+  @user_collection = current_user.collections.first
+  if @card = Card.create(user_id: current_user.id, species_id: params[:species_id], collection_id: params[:collection])
     content_type :json
     @card.to_json
   else
